092cd7a5a2c67da23f3f7345ec7f8d3648750980
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-24 09:29:21 +0300

    commit tree ee26e06b0c87d3ee1bdf53af2c471892b48b1895

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(message= "roller-skate"){
+    return `This Saturday, I want to ${message}!`;
+
+
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(message ='go to the office'){
+
+   
+    return `This Monday, I will ${message}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(name = "*") {
+   
+    return function(adjective = "special") {
+        return `You are ${name}${adjective}${name}!`;
+    }
+}
